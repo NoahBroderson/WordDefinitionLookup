f21c4437474199d5b921de7fe46b0fd056fa931d
nbroderson <nbroderson@RTI-025369>
2016-09-24 07:19:32 +0200

    Reorganized into separate projects

--- WordLookup.UI/Program.cs
+++ WordLookup.UI/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WordDefinitionLookup
+namespace WordLookup.UI
 {
     static class Program
     {
@@ -16,8 +16,7 @@ namespace WordDefinitionLookup
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            Application.Run(new frmWordLookup());
+            Application.Run(new Form1());
         }
     }
 }
